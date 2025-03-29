727c0040bde9c45c2cc4296c969f81eba3530563
s4055 <sijin901@gmail.com>
2025-03-29 21:48:12 +0900

    feat: project init
    feat: project init

---
+++ src/test/java/jwt/auth/demo/JwtAuthDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package jwt.auth.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthDemoApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
