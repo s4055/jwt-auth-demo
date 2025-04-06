dc191063826c10c5b22b5a9bfaf76186f079964b
s4055 <sijin901@gmail.com>
2025-04-06 21:54:52 +0900

    feat: jwt filter 수정
    feat: jwt filter 수정

--- src/main/java/jwt/auth/demo/filter/JwtFilter.java
+++ src/main/java/jwt/auth/demo/filter/JwtFilter.java
@@ -1,12 +1,13 @@
 package jwt.auth.demo.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import jwt.auth.demo.dto.common.CommonResponse;
-import jwt.auth.demo.exception.CustomException;
 import jwt.auth.demo.exception.ErrorCode;
 import jwt.auth.demo.util.JwtUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -18,26 +19,24 @@ public class JwtFilter extends OncePerRequestFilter {
 
   @Override
   protected void doFilterInternal(
-      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
 
-    try {
-      if (ignoreURI(request)) {
-        filterChain.doFilter(request, response);
-        return;
-      }
-
-      String token = request.getHeader("Authorization");
-      if (token == null || !JwtUtil.validateToken(token.replace("Bearer ", ""))) {
-        throw new CustomException(ErrorCode.UNAUTHORIZED_TOKEN);
-      }
-
-      String email = JwtUtil.getEmail(token.replace("Bearer ", ""));
-      request.setAttribute("email", email);
-
+    if (ignoreURI(request)) {
       filterChain.doFilter(request, response);
-    } catch (Exception e) {
-      throw new RuntimeException();
+      return;
     }
+
+    String token = request.getHeader("Authorization");
+    if (token == null || !JwtUtil.validateToken(token.replace("Bearer ", ""))) {
+      sendErrorResponse(response);
+      return;
+    }
+
+    String email = JwtUtil.getEmail(token.replace("Bearer ", ""));
+    request.setAttribute("email", email);
+
+    filterChain.doFilter(request, response);
   }
 
   private boolean ignoreURI(HttpServletRequest request) {
@@ -47,10 +46,14 @@ public class JwtFilter extends OncePerRequestFilter {
     };
   }
 
-//  private void sendErrorResponse(HttpServletResponse response) throws IOException {
-//    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-//    response.getWriter().write(String.valueOf(new CommonResponse(ErrorCode.UNAUTHORIZED_TOKEN)));
-//  }
+  private void sendErrorResponse(HttpServletResponse response) throws IOException {
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+    response
+        .getWriter()
+        .write(
+            new ObjectMapper()
+                .writeValueAsString(new CommonResponse(ErrorCode.UNAUTHORIZED_TOKEN)));
+  }
 }
