57302ab25e5b128dbd04999cc821afc9ac395b65
s4055 <sijin901@gmail.com>
2025-04-08 00:54:04 +0900

    feat: swagger config 수정
    feat: swagger config 수정

--- src/main/java/jwt/auth/demo/config/SwaggerConfig.java
+++ src/main/java/jwt/auth/demo/config/SwaggerConfig.java
@@ -11,22 +11,43 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-  private static final String JWT = "jwtAuth";
+  private static final String JWT = "JWT";
+  private static final String COOKIE = "COOKIE";
 
   @Bean
   public OpenAPI openAPI() {
-    Info info = new Info().title("jwt demo").version("0.0.1").description("JWT 토큰 연습");
-    SecurityRequirement securityRequirement = new SecurityRequirement().addList(JWT);
-    Components components =
-        new Components()
-            .addSecuritySchemes(
-                "jwtAuth",
-                new SecurityScheme()
-                    .name("jwtAuth")
-                    .type(SecurityScheme.Type.HTTP)
-                    .scheme("bearer")
-                    .bearerFormat("JWT"));
+    return new OpenAPI()
+        .info(info())
+        .addSecurityItem(securityRequirement())
+        .components(securityComponents());
+  }
 
-    return new OpenAPI().info(info).addSecurityItem(securityRequirement).components(components);
+  private Info info() {
+    return new Info().title("jwt demo").version("0.0.1").description("JWT 토큰 연습");
+  }
+
+  private SecurityRequirement securityRequirement() {
+    return new SecurityRequirement().addList(JWT).addList(COOKIE);
+  }
+
+  private Components securityComponents() {
+    return new Components()
+        .addSecuritySchemes(JWT, jwtScheme())
+        .addSecuritySchemes(COOKIE, cookieScheme());
+  }
+
+  private SecurityScheme jwtScheme() {
+    return new SecurityScheme()
+        .name(JWT)
+        .type(SecurityScheme.Type.HTTP)
+        .scheme("bearer")
+        .bearerFormat("JWT");
+  }
+
+  private SecurityScheme cookieScheme() {
+    return new SecurityScheme()
+        .name(COOKIE)
+        .type(SecurityScheme.Type.APIKEY)
+        .in(SecurityScheme.In.COOKIE);
   }
 }
